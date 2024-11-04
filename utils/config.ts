7856e663c76f00c4c5a7a59e655c0cbb21726067
Lữ Minh Khương <13156@student.vgu.edu.vn>
2024-11-04 21:40:26 +0700

    update baseurl

--- utils/config.ts
+++ utils/config.ts
@@ -1,14 +1,14 @@
-export const BASEURL = "https://leetcode-badge-showcase.vercel.app";
+export const BASEURL = "https://luminhkhuong-leetcode-badge-showcase.vercel.app";
 
 export const LEETCODE_BASEURL = "https://leetcode.com";
 
 export const THEME_NAMES = ['light', 'dark', 'sky', 'beach', 'github-dark', 'nightowl',
-    'tokyonight', 'onedark', 'dracula', 'monokai', 'shades-of-purple', 'cobalt2', 'mint', 'purple-gang', 'leafy', 'black'];
+	'tokyonight', 'onedark', 'dracula', 'monokai', 'shades-of-purple', 'cobalt2', 'mint', 'purple-gang', 'leafy', 'black'];
 
 export const FILTERS = {
-    daily: 'DCC',
-    comp: 'COMPETITION',
-    study: 'STUDY_PLAN'
+	daily: 'DCC',
+	comp: 'COMPETITION',
+	study: 'STUDY_PLAN'
 }
 
 export const BORDER = ['border', 'no-border']
